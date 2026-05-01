0a42ebc0e4837963a14eea97e93e4213dca67e32
Niklas Deutschmann <sonstharmlos@noreply.codeberg.org>
2026-05-01 16:25:32 +0200

    Paging in activity details view ("Back to Activities" link should work now)

--- src/main/java/net/javahippie/fitpub/controller/ActivitiesViewController.java
+++ src/main/java/net/javahippie/fitpub/controller/ActivitiesViewController.java
@@ -1,9 +1,11 @@
 package net.javahippie.fitpub.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * Controller for serving activity-related HTML pages
@@ -32,8 +34,11 @@ public class ActivitiesViewController {
      * Show activity detail page
      */
     @GetMapping("/{id}")
-    public String viewActivity(@PathVariable String id) {
+    public String viewActivity(@PathVariable String id, @RequestParam String page, Model model) {
         // The activity data will be loaded via JavaScript API calls
+        if (page != null) {
+            model.addAttribute("page", page);
+        }
         return "activities/detail";
     }
 
